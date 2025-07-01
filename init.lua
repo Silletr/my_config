8acd7c20a386c25dff019710b0ce56728ca686bc
Silletr <153998312+Silletr@users.noreply.github.com>
2025-07-01 11:19:18 +0300

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,144 @@
+vim.g.python3_host_prog = '/usr/bin/python3.10'
+
+-- включаем полноцветность
+vim.o.termguicolors = true
+vim.o.background = "dark"
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.clipboard = 'unnamedplus'
+
+vim.g.mapleader = ' '  -- leader = space
+
+local packer_ok, packer = pcall(require, 'packer')
+if not packer_ok then
+  vim.notify("Packer not found!", vim.log.levels.ERROR)
+  return
+end
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- 🎨 Theme
+  use {
+    'navarasu/onedark.nvim',
+    config = function()
+      require('onedark').setup { style = 'deep' }
+      require('onedark').load()
+    end,
+  }
+
+  -- 📝 Autocomplete
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  -- UI
+  use 'sheerun/vim-polyglot'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+
+ -- File tree
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup({
+        renderer = {
+          add_trailing = false,
+          group_empty = true,
+          highlight_git = true,
+          root_folder_modifier = ':t',
+          indent_markers = {
+            enable = true,
+            icons = {
+              corner = '└',
+              edge = '│',
+              item = '│',
+              none = ' ',
+            },
+          },
+          icons = {
+            glyphs = {
+              default = '',
+              symlink = '',
+              folder = {
+                default = '',
+                open = '',
+                empty = '',
+                empty_open = '',
+                symlink = '',
+              },
+              git = {
+                unstaged = '✗',
+                staged = '✓',
+                unmerged = '',
+                renamed = '➜',
+                deleted = '',
+                ignored = '◌',
+              },
+            },
+          },
+        },
+        diagnostics = {
+          enable = true,
+          show_on_dirs = true,
+          icons = {
+            error = 'E',
+            warning = 'W',
+            hint = 'H',
+            info = 'I',
+          },
+        },
+        update_focused_file = {
+          enable = true,
+          update_cwd = true,
+          ignore_list = {},
+        },
+        filters = {
+          dotfiles = false,
+          custom = {},
+          exclude = {'.git', 'node_modules', '.cache'},
+        },
+        git = {
+          enable = true,
+          ignore = true,
+          timeout = 400,
+        },
+        trash = {
+          cmd = 'trash',
+          require_confirm = true,
+        },
+        actions = {
+          open_file = {
+            quit_on_open = false,
+            resize_window = true,
+          },
+        },
+      })
+    end,
+  }
+end)
+
+vim.api.nvim_create_user_command('CheckPython', function()
+    local has_python = vim.fn.has('python3')
+    local python_path = vim.g.python3_host_prog
+    vim.notify(string.format('Python3 support: %s\nPython path: %s', 
+        has_python == 1 and '✓ Available' or '✗ Not available',
+        python_path or 'Not configured'
+    ))
+end, {})
+
+
+vim.cmd [[colorscheme onedark]]
+local lazy_ok, lazy = pcall(require, "LazyDevHelper.plugin.commands")
+if lazy_ok and lazy.commands then lazy.commands() end
+
+vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap=true, silent=true })
+vim.keymap.set("n", "<C-g>", ":NvimTreeFindFile<CR>", { noremap=true, silent=true })
+
