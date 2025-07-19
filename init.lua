939d5c58837ce42f2567bc0de33eb5c712382a90
Silletr <153998312+Silletr@users.noreply.github.com>
2025-07-19 23:04:18 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -33,15 +33,72 @@ packer.startup(function(use)
         }
     end
     }
-  -- Myself plugin
+
+  -- my plugin
   use {
-    'Silletr/LazyDevHelper',
+    'Silletr/LazyDeveloperHelper',
     config = function()
       require("LazyDeveloperHelper")
     end
   }
 
-  -- 🎨 Theme
+  -- LSP
+  use {
+     'williamboman/mason.nvim',
+  }
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'https://github.com/neovim/nvim-lspconfig'
+
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp',
+      'L3MON4D3/LuaSnip',      
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets', 
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup{
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>']     = cmp.mapping.confirm({ select = true }),
+          ['<Tab>']    = cmp.mapping(function(fallback)
+                            if cmp.visible() then cmp.select_next_item()
+                            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+                            else fallback() end
+                          end, { 'i','s' }),
+          ['<S-Tab>']  = cmp.mapping(function(fallback)
+                            if cmp.visible() then cmp.select_prev_item()
+                            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+                            else fallback() end
+                          end, { 'i','s' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      }
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = 'buffer' } }
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } })
+      })
+    end
+  }
+ -- 🎨 Theme
   use {
     'folke/tokyonight.nvim',
     config = function()
@@ -138,6 +195,50 @@ packer.startup(function(use)
   }
 
 end)
+
+require('mason').setup()
+require('mason-lspconfig').setup {
+    ensure_installed = { 'pyright', 'lua_ls' },
+    automatic_installation = false, 
+  }
+local lspconfig = require('lspconfig')
+local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true, buffer=bufnr }
+
+  -- Примеры полезных хоткеев LSP
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts) 
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting or vim.lsp.buf.format, opts)
+end
+
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  flags = { debounce_text_changes = 150 },
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      }
+    }
+  }
+}
+
+lspconfig.lua_ls.setup {
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
+      diagnostics = { globals = {'vim', 'use'} },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+      telemetry = { enable = false },
+    }
+  }
+}
 
 vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap=true, silent=true })
 vim.keymap.set("n", "<C-g>", ":NvimTreeFindFile<CR>", { noremap=true, silent=true })
